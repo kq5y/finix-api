5fc4e36f46fcaf0746620fe5138829cbc704d19e
kq5y <tkser.edu@gmail.com>
2025-03-06 02:40:44 +0900

    improve: add payment type validation to PaymentMethod model

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,6 +1,7 @@
 class PaymentMethod < ApplicationRecord
   VALID_SORT_ORDERS = %w[asc desc].freeze
   VALID_SORT_KEYS = %w[id].freeze
+  VALID_PAYMENT_TYPES = %w[cash card qrcode other].freeze
 
   def as_json(options = {})
     super(options.merge(except: [ :user_id ]))
@@ -9,6 +10,7 @@ class PaymentMethod < ApplicationRecord
   belongs_to :user
   has_many :expenditures
   validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :payment_type, inclusion: { in: VALID_PAYMENT_TYPES }, allow_nil: true
 
   scope :apply_sort, ->(sort_key, sort_order) {
     sort_key = sort_key.presence_in(VALID_SORT_KEYS) || "id"
