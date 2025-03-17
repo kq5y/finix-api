6d004db27cd6b59fd1d2998586e6d4ddcf5ac0a5
kq5y <tkser.edu@gmail.com>
2025-03-18 02:09:53 +0900

    fix: add unique index on uid column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_06_042053) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_170409) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_042053) do
     t.datetime "updated_at", null: false
     t.datetime "discarded_at"
     t.index ["discarded_at"], name: "index_users_on_discarded_at"
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   add_foreign_key "categories", "users"
